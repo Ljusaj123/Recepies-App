14150b5f75d7ab1bf47a1cd6b2565f273c2eabee
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-04-23 01:13:28 +0200

    implemented router for pages

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Explore() {
   return (
@@ -7,21 +8,29 @@ function Explore() {
         <div className="container">
           <div className="title-container">
             <h1>Food</h1>
-            <button className="explore-button">Explore</button>
+            <button className="explore-button">
+              {" "}
+              <Link to="/food">Explore</Link>
+            </button>
           </div>
           <img src="/pictures/bread-g32496b22f_1920.jpg" alt="" />
         </div>
         <div className="container">
           <div className="title-container">
             <h1>Cocktail</h1>
-            <button className="explore-button">Explore</button>
+            <button className="explore-button">
+              {" "}
+              <Link to="/cocktail">Explore</Link>
+            </button>
           </div>
           <img src="/pictures/pexels-live-on-shot-2789328.jpg" alt="" />
         </div>
         <div className="container">
           <div className="title-container">
             <h1>Beer</h1>
-            <button className="explore-button">Explore</button>
+            <button className="explore-button">
+              <Link to="/beer">Explore </Link>
+            </button>
           </div>
           <img src="/pictures/jon-parry-C8eSYwQkwHw-unsplash.jpg" alt="" />
         </div>
