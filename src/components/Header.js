f5434e47e0450cd46c814977aadf019c086f71b5
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-04-22 16:10:04 +0200

    component for header

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="header-container">
+        <div className="logo-container">
+          <h1>Tasty Recepies</h1>
+        </div>
+        <div className="header-info">
+          <h2>
+            Get yourself the best recepies inspiration for lunch! Or party
+          </h2>
+          <button>Explore</button>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
