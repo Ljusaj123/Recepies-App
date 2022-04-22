8613c804ce2bc2def1630dad3ab33def437e9728
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-04-22 17:45:44 +0200

    jsx for Explore component

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,7 +1,33 @@
 import React from "react";
 
 function Explore() {
-  return <div>Explore</div>;
+  return (
+    <section className="explore">
+      <div className="explore-container">
+        <div className="container">
+          <div className="title-container">
+            <h1>Food</h1>
+            <button className="explore-button">Explore</button>
+          </div>
+          <img src="/pictures/bread-g32496b22f_1920.jpg" alt="" />
+        </div>
+        <div className="container">
+          <div className="title-container">
+            <h1>Coctail</h1>
+            <button className="explore-button">Explore</button>
+          </div>
+          <img src="/pictures/pexels-live-on-shot-2789328.jpg" alt="" />
+        </div>
+        <div className="container">
+          <div className="title-container">
+            <h1>Beer</h1>
+            <button className="explore-button">Explore</button>
+          </div>
+          <img src="/pictures/jon-parry-C8eSYwQkwHw-unsplash.jpg" alt="" />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Explore;
