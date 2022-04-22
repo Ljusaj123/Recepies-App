5412a5f1f1fe0d550ada9c36eb1fea2ab81ce1d0
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-04-23 00:24:18 +0200

    spelling mistake

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,11 @@ function Header() {
     <header>
       <div className="header-container">
         <div className="logo-container">
-          <h1>Tasty Recepies</h1>
+          <h1>Tasty Recipes</h1>
         </div>
         <div className="header-info">
           <h2>
-            Get yourself the best recepies inspiration for lunch! Or party!
+            Get yourself the best recipes inspiration for lunch! Or party!
           </h2>
           <button className="explore-button">
             <a href="#explore">Explore</a>
