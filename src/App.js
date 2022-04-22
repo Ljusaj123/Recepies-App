803dec628318dd02bc57f80f6afbe7038d051a31
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-04-22 16:55:31 +0200

    created explore component, imported into APP file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Explore from "./components/Explore";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Explore />
     </div>
   );
 }
