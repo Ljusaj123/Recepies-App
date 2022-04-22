8eca5ec4aa8862e475538618464156d02b010162
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-04-23 00:23:41 +0200

    created Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-container">
+        <p>Please take a look!</p>
+        <p>
+          This app uses beer, food and cocktail API to provide recepies, where
+          user can search, filter and bookmark recipes.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
