bf31d5e696dd511d60e1acce6c1adcad779caa0a
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-04-23 00:23:59 +0200

    included Footer component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Explore from "./components/Explore";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <Header />
       <Explore />
+      <Footer />
     </div>
   );
 }
