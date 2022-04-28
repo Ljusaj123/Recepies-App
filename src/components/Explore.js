97727eb1195041622f8c898a55c9e6a73134f371
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-04-28 21:44:12 +0200

    included categories and recepies list component

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import Categories from "./Categories";
+import RecepiesList from "./RecepiesList";
 
 function Explore() {
+  const [meals, setMeals] = useState([]);
+
   return (
     <section className="explore" id="explore">
-      <div className="explore-container"></div>
+      <div className="explore-container">
+        <h1>Select Category:</h1>
+        <Categories meals={meals} setMeals={setMeals} />
+        <RecepiesList meals={meals} />
+      </div>
     </section>
   );
 }
