d5977740653ede2055809a4488c4cc2f3b644f9b
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-04-29 01:14:42 +0200

    created pages folder for home and single meal

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Explore from '../components/Explore'
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Explore />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
