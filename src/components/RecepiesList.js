702a9f30372d25da5e09219d6bd5eef255eda893
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-04-29 19:27:17 +0200

    implemented error component

--- src/components/RecepiesList.js
+++ src/components/RecepiesList.js
@@ -1,22 +1,25 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import MealsContext from "../contexts/MealsContext";
+import Error from "./Error";
 
 function RecepiesList() {
-  const { meals } = useContext(MealsContext);
+  const { meals, errorMeals } = useContext(MealsContext);
 
   return (
     <div className="recepies-list" id="list">
-      {meals.map((meal, index) => {
-        return (
-          <Link to={`/meals/${meal.idMeal}`}>
-            <div className="recepie" key={index}>
-              <h3>{meal.strMeal}</h3>
-              <img src={meal.strMealThumb} alt="" />
-            </div>
-          </Link>
-        );
-      })}
+      {errorMeals && <Error error={errorMeals} />}
+      {meals &&
+        meals.map((meal, index) => {
+          return (
+            <Link to={`/meals/${meal.idMeal}`}>
+              <div className="recepie" key={index}>
+                <h3>{meal.strMeal}</h3>
+                <img src={meal.strMealThumb} alt="" />
+              </div>
+            </Link>
+          );
+        })}
     </div>
   );
 }
