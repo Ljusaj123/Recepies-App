ba3679e0ca403559f6025d33a3caa7984626f192
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-05-03 00:29:13 +0200

    set loading and error

--- src/components/RecepiesList.js
+++ src/components/RecepiesList.js
@@ -2,24 +2,39 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import MealsContext from "../contexts/MealsContext";
 import Error from "./Error";
+import { HalfMalf } from "react-spinner-animated";
+import "react-spinner-animated/dist/index.css";
 
 function RecepiesList() {
-  const { meals, errorMeals } = useContext(MealsContext);
+  const { meals, error, isLoading } = useContext(MealsContext);
 
+  if (error.isError) {
+    return <Error error={error.msg} />;
+  }
+
+  if (isLoading) {
+    return (
+      <HalfMalf
+        text={"Loading..."}
+        bgColor={"#ddaaaa1a"}
+        center={true}
+        width={"130px"}
+        height={"130px"}
+      />
+    );
+  }
   return (
-    <div className="recepies-list" id="list">
-      {errorMeals && <Error error={errorMeals} />}
-      {meals &&
-        meals.map((meal, index) => {
-          return (
-            <Link to={`/meals/${meal.idMeal}`}>
-              <div className="recepie" key={index}>
-                <h3>{meal.strMeal}</h3>
-                <img src={meal.strMealThumb} alt="" />
-              </div>
-            </Link>
-          );
-        })}
+    <div className="recepies-list">
+      {meals.map((meal, index) => {
+        return (
+          <Link to={`/meals/${meal.idMeal}`} key={index}>
+            <div className="recepie">
+              <h3>{meal.strMeal}</h3>
+              <img src={meal.strMealThumb} alt="" />
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 }
