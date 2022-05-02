e183f3f6884e948f61f61351c0a7bb21419595e1
Ljusaj123 <rosana.ljusaj2@gmail.com>
2022-05-03 00:28:44 +0200

    used custom hook, set loading and error component, cleaner code

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,73 +1,64 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import MealsContext from "../contexts/MealsContext";
 import { HalfMalf } from "react-spinner-animated";
+import "react-spinner-animated/dist/index.css";
 import Error from "./Error";
 import getData from "../API calls/getData";
+import useGetData from "../API calls/useGetData";
 
 function Categories() {
-  const { setMeals, setErrorMeals } = useContext(MealsContext);
+  const { setMeals, setError, setIsLoading } = useContext(MealsContext);
   const CategoriesListURL = `https://www.themealdb.com/api/json/v1/1/list.php?c=list`;
 
-  const [categories, setCategories] = useState([]);
-  const [isLoading, setIsloading] = useState(true);
-  const [errorCat, setErrorCat] = useState(null);
-
-  useEffect(() => {
-    setIsloading(true);
-    getData(CategoriesListURL)
-      .then((data) => {
-        setCategories(data);
-        setIsloading(false);
-        setErrorCat(null);
-      })
-      .catch((error) => {
-        setErrorCat(error.message);
-        setIsloading(false);
-      });
-  }, [CategoriesListURL]);
+  const { meal: categories, error, isLoading } = useGetData(CategoriesListURL);
 
   const getMeals = (query) => {
     const MealsByCatURL = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${query}`;
-    setIsloading(true);
+    setIsLoading(true);
     getData(MealsByCatURL)
       .then((data) => {
         setMeals(data);
-        setErrorMeals(null);
       })
       .catch((error) => {
-        setErrorMeals(error.message);
+        setError({
+          isError: true,
+          msg: error.message,
+        });
       })
       .finally(() => {
-        setIsloading(false);
+        setIsLoading(false);
       });
   };
 
+  if (isLoading) {
+    return (
+      <HalfMalf
+        text={"Loading..."}
+        bgColor={"#ddaaaa1a"}
+        center={true}
+        width={"130px"}
+        height={"130px"}
+      />
+    );
+  }
+
+  if (error.isError) {
+    return <Error error={error.msg} />;
+  }
+
   return (
     <div className="categories">
-      {isLoading && (
-        <HalfMalf
-          text={"Loading..."}
-          bgColor={"#ddaaaa1a"}
-          center={true}
-          width={"130px"}
-          height={"130px"}
-        />
-      )}
-      {errorCat && <Error error={errorCat} />}
-      {categories &&
-        categories.map((category, index) => {
-          return (
-            <div
-              className="category"
-              key={index}
-              onClick={() => getMeals(category.strCategory)}
-            >
-              <h3>
-                <a href="#list">{category.strCategory}</a>
-              </h3>
-            </div>
-          );
-        })}
+      {categories.map((category, index) => {
+        return (
+          <div
+            className="category"
+            key={index}
+            onClick={() => getMeals(category.strCategory)}
+          >
+            <h3>{category.strCategory}</h3>
+          </div>
+        );
+      })}
     </div>
   );
 }
